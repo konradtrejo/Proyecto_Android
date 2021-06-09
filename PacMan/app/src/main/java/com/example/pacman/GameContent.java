5e4e14c0806c72ae87b9f0e6a13ee9f32fc1ff51
konradtrejo <konrad.trejo.c@uni.pe>
2021-06-09 02:02:38 -0500

    modified and load music

--- PacMan/app/src/main/java/com/example/pacman/GameContent.java
+++ PacMan/app/src/main/java/com/example/pacman/GameContent.java
@@ -1,6 +1,7 @@
 package com.example.pacman;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -10,12 +11,21 @@ import com.example.pacman.databinding.GameContentBinding;
 public class GameContent extends Activity {
 
     private GameContentBinding binding;
-
+    private MediaPlayer player;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_content);
         setContentView(new PacMan(getApplicationContext()));
         System.out.println("Game Content");
+        loadMusic();
+    }
+    public void   loadMusic (){
+
+        player = MediaPlayer.create(this, R.raw.pacman_song);
+        player.setVolume(100, 100);
+        player.setLooping(true);
+        player.start();
+
     }
 }
